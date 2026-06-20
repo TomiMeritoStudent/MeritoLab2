15bd268dde97ee6c7dae8a0b531ac15b43d4f79e
TomiStudent <wrx98315@student.wroclaw.merito.pl>
2026-06-20 23:18:46 +0200

    dodano dokumentację do aplikacji

--- app/calculator.py
+++ app/calculator.py
@@ -1,2 +1,16 @@
+"""
+Module providing basic calculator functions.
+"""
+
 def add(a, b):
+    """
+    Add two numbers and return the result.
+
+    Args:
+        a (int | float): First number
+        b (int | float): Second number
+
+    Returns:
+        int | float: Sum of a and b
+    """
     return a + b
